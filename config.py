255c85a39c584125a0743daaa54e2c20398c4e10
ariane.rodrigues <ariane.rodrigues@smartbreeder.com>
2026-08-28 14:08:30 -0300

    config.cvs arrumado

--- config.py
+++ config.py
@@ -6,15 +6,6 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent
 
-
-DB_SERVER = os.getenv("DB_SERVER")
-DB_DATABASE = os.getenv("DB_DATABASE")
-DB_DRIVER = os.getenv("DB_DRIVER", "ODBC Driver 18 for SQL Server")
-DB_TRUSTED_CONNECTION = os.getenv("DB_TRUSTED_CONNECTION", "yes")  
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_TRUST_SERVER_CERTIFICATE = os.getenv("DB_TRUST_SERVER_CERTIFICATE", "yes")
-
 AZURE_TENANT_ID = os.getenv("AZURE_TENANT_ID")
 AZURE_CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
 AZURE_CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
